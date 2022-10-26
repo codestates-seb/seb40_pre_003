0d026ec8ee12961620360eef08bc1ec6aec988f8
callmejeje <choije401@naver.com>
2022-10-26 23:25:42 +0900

    feat: Side Navigation Bar 로그인, 회원가입, 질문 페이지에서 안보이게 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import GlobalStyle from './assets/GlobalStyle';
 import Footer from './components/Layout/Footer';
@@ -6,30 +6,35 @@ import GlobalNav from './components/Layout/GlobalNav';
 import SideNav from './components/Layout/SideNav';
 
 function App() {
+  const { pathname } = useLocation();
+
+  const noSnb = ['/ask', '/login', '/logout', '/signup'];
+  const noFooter = ['/login', '/logout', '/signup'];
+
+  const hideSnb = noSnb.includes(pathname);
+  const hideFooter = noFooter.includes(pathname);
   return (
     <>
-      <BrowserRouter>
-        <GlobalStyle />
-        <GlobalNav />
-        <Body>
-          <SideNav />
-          <Routes>
-            <Route path="/" element={<div>home</div>} />
-            <Route path="/signup" element={<div>signup</div>} />
-            <Route path="/login" element={<div>login</div>} />
-            <Route path="/questions" element={<div>questions</div>} />
-            <Route path="/questions/ask" element={<div>ask</div>} />
-            <Route path={`/questions/:id`} element={<div>questions/:id</div>} />
-            {/* querystring으로 검색 결과 페이지 이동 (/search?q=springboot) */}
-            <Route path="/search" element={<div>search result</div>} />
-            <Route path="/tags" element={<div>tags</div>} />
-            <Route path="/users" element={<div>users</div>} />
-            {/* id가 본인이면 마이페이지 */}
-            <Route path={`/users/:id`} element={<div>users/:id</div>} />
-          </Routes>
-        </Body>
-        <Footer />
-      </BrowserRouter>
+      <GlobalStyle />
+      <GlobalNav />
+      <Body>
+        {hideSnb || <SideNav />}
+        <Routes>
+          <Route path="/" element={<div>home</div>} />
+          <Route path="/signup" element={<div>signup</div>} />
+          <Route path="/login" element={<div>login</div>} />
+          <Route path="/questions" element={<div>questions</div>} />
+          <Route path="/questions/ask" element={<div>ask</div>} />
+          <Route path={`/questions/:id`} element={<div>questions/:id</div>} />
+          {/* querystring으로 검색 결과 페이지 이동 (/search?q=springboot) */}
+          <Route path="/search" element={<div>search result</div>} />
+          <Route path="/tags" element={<div>tags</div>} />
+          <Route path="/users" element={<div>users</div>} />
+          {/* id가 본인이면 마이페이지 */}
+          <Route path={`/users/:id`} element={<div>users/:id</div>} />
+        </Routes>
+      </Body>
+      {hideFooter || <Footer />}
     </>
   );
 }
