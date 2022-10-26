51a4873d26aedc41b244e05289f08ff5b2265656
callmejeje <choije401@naver.com>
2022-10-26 16:37:11 +0900

    style: css 수정

--- client/src/components/Layout/SideNav/style.js
+++ client/src/components/Layout/SideNav/style.js
@@ -1,3 +1,4 @@
+import { IoEarthSharp } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -15,22 +16,67 @@ export const HomeLink = styled(Link)`
   width: 152px;
   height: 26px;
   padding: 4px 4px 4px 8px;
+  color: var(--black-600);
+  font-weight: 500;
+  :hover {
+    color: var(--black-800);
+  }
+`;
+
+export const MyIoEarthSharp = styled(IoEarthSharp)`
+  margin-right: 4px;
+`;
+
+export const Questions = styled.span`
+  padding: 4px 8px 4px 0px;
 `;
 
 export const PublicList = styled.div`
   display: flex;
   align-items: center;
-  padding: 4px 4px 4px 30px;
+  padding: 4px 8px 4px 0px;
+
+  /* padding: 4px 4px 4px 30px; */
 `;
+
+export const QuestionsLink = styled(Link)`
+  padding: 4px 4px 4px 8px;
+  display: flex;
+  align-items: center;
+  font-size: 13px;
+  text-decoration: none;
+  color: var(--black-600);
+
+  border-right: 3px;
+  width: 130px;
+  height: 26px;
+
+  :hover {
+    color: var(--black-900);
+  }
+`;
+
 export const PublicLink = styled(Link)`
   display: flex;
   align-items: center;
   font-size: 13px;
   text-decoration: none;
+  color: var(--black-600);
   /* padding: 8px 6px 8px 8px; */
+  padding: 4px 4px 4px 30px;
+
   border-right: 3px;
   width: 130px;
   height: 26px;
+
+  :hover {
+    color: var(--black-900);
+  }
+`;
+
+export const Ol = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
 
 export const List = styled.div`
@@ -38,8 +84,18 @@ export const List = styled.div`
   flex-direction: column;
   font-size: 11px;
   /* margin: 16px 0 4px 8px; */
+  height: 100%;
 `;
 
 export const Name = styled.div`
   margin: 16px 0 4px 8px;
+  color: var(--fc-light);
+  height: 100%;
+`;
+
+export const End = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* flex: 1 1 auto; */
+  height: 600px;
 `;
