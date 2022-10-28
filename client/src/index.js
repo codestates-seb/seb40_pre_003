825f828fb75ba2e256a638151c78f18e422c5fe5
ALSRUD29 <dev.mnkg@gmail.com>
2022-10-28 14:08:52 +0900

    create redux, provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import store from './redux/index';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
