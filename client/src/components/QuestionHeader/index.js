8a65aec13e44bc039e3937579fd042784e5de90b
callmejeje <choije401@naver.com>
2022-10-28 14:13:23 +0900

    feat: 질문 상세 페이지 제목 부분 모양 만듦

---
+++ client/src/components/QuestionHeader/index.js
@@ -0,0 +1,37 @@
+import AskQuestionButton from '..//Buttons/AskQuestionButton';
+import {
+  Container,
+  Figure,
+  FigureContent,
+  FigureName,
+  FiguresContainer,
+  Title,
+  TitleContainer,
+} from './style';
+
+function QuestionHeader({ testdata }) {
+  return (
+    <Container>
+      <TitleContainer>
+        <Title>{testdata.title}</Title>
+        <AskQuestionButton />
+      </TitleContainer>
+      <FiguresContainer>
+        <Figure>
+          <FigureName>Asked</FigureName>
+          <FigureContent>today</FigureContent>
+        </Figure>
+        <Figure>
+          <FigureName>Modified</FigureName>
+          <FigureContent>today</FigureContent>
+        </Figure>
+        <Figure>
+          <FigureName>Viewed</FigureName>
+          <FigureContent>{`${testdata.views} times`}</FigureContent>
+        </Figure>
+      </FiguresContainer>
+    </Container>
+  );
+}
+
+export default QuestionHeader;
---
+++ client/src/components/QuestionHeader/style.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const TitleContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  /* align-items: center; */
+  height: auto;
+  /* flex: 1 1 auto; */
+`;
+
+export const Title = styled.h1`
+  display: flex;
+  font-size: 27px;
+  font-weight: normal;
+  margin-bottom: 8px;
+  height: auto;
+`;
+
+export const FiguresContainer = styled.section`
+  display: flex;
+  height: 25;
+  padding-bottom: 8px;
+  border-bottom: 1px solid var(--black-075);
+  margin-bottom: 16px;
+`;
+
+export const Figure = styled.div`
+  height: 17px;
+  margin: 0 16px 8px 0;
+  font-weight: 500;
+`;
+
+export const FigureName = styled.span`
+  font-size: 13px;
+  color: var(--fc-light);
+  margin-right: 2px;
+  font-weight: inherit;
+`;
+
+export const FigureContent = styled.span`
+  font-size: 13px;
+  color: #232629;
+  margin-left: 2px;
+  font-weight: inherit;
+`;
