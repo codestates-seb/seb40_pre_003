2e0f3f79ed112781de1275cf009d42ad68896c57
ALSRUD29 <dev.mnkg@gmail.com>
2022-10-28 13:02:34 +0900

    feat: 로그아웃UI완성

--- client/src/pages/Register/Logout/index.js
+++ client/src/pages/Register/Logout/index.js
@@ -1,99 +1,12 @@
-import styled from 'styled-components';
+import ubuntu from '../../../../src/assets/images/askubuntu.png';
+import mathoverflow from '../../../../src/assets/images/mathoverflow.png';
+import serverfault from '../../../../src/assets/images/serverfault.png';
+import stackapps from '../../../../src/assets/images/stackapps.png';
+import stackexchange from '../../../../src/assets/images/stackexchange.png';
+import stackoverflow from '../../../../src/assets/images/stackoverflow.png';
+import superuser from '../../../../src/assets/images/superuser.png';
 
-const Container = styled.div`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background-color: #f1f2f3;
-
-  > div.logout_guide {
-    font-size: 28px;
-    font-weight: 16px;
-    margin: 20px;
-    text-align: center;
-    //텍스트 줄간격 벌리기??
-  }
-`;
-
-const LogoutBlock = styled.div`
-  margin: 10px;
-  width: 300px;
-  height: 430px;
-  padding: 24px;
-  border-radius: 10px;
-  background-color: white;
-  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-
-  > div.linked_site {
-    color: hsl(206, 100%, 40%);
-    font-size: 15px;
-    margin: 4px;
-    padding: 4px;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    font-weight: 600;
-  }
-
-  > div#checkbox {
-    font-size: 14px;
-    padding: 2px 0px 0px;
-    font-weight: 600;
-    margin: 20px 0px;
-    > span {
-      margin-left: 5px;
-    }
-  }
-
-  > div#buttons {
-    > button#logout_button {
-      margin-top: 15px;
-      width: 80px;
-      height: 50px;
-      margin: 2px;
-      padding: 10px;
-      background-color: #0a95ff;
-      border: none;
-      border-radius: 5px;
-      font-size: 14px;
-      font-weight: 600;
-      color: white;
-      cursor: pointer;
-      :hover {
-        background-color: #0074cc;
-      }
-    }
-
-    > button#cancel_button {
-      margin-top: 15px;
-      width: 80px;
-      height: 50px;
-      margin: 2px;
-      padding: 10px;
-      background-color: white;
-      border: none;
-      border-radius: 5px;
-      font-size: 14px;
-      font-weight: 600;
-      color: #0a95ff;
-      cursor: pointer;
-      :hover {
-        background-color: hsl(205, 46%, 92%);
-        //옅은 하늘색으로 바뀌기
-      }
-    }
-  }
-
-  > div#logout_all_device_text {
-    font-size: 14px;
-    margin: 30px 3px 5px 5px;
-    padding: 2px;
-    color: #6a737c;
-    font-weight: 400;
-  }
-`;
+import { Container, Logo, LogoutBlock } from './style';
 
 export default function Logout() {
   return (
@@ -103,13 +16,35 @@ export default function Logout() {
         this device:
       </div>
       <LogoutBlock>
-        <div className="linked_site">askubuntu.com</div>
-        <div className="linked_site">mathoverflow.net</div>
-        <div className="linked_site">serverfault.com</div>
-        <div className="linked_site">stackapp.com</div>
-        <div className="linked_site">stackexchange.com</div>
-        <div className="linked_site">stackoverflow.com</div>
-        <div className="linked_site">superuser.com</div>
+        <div className="linked_site">
+          <Logo src={ubuntu} />
+          askubuntu.com
+        </div>
+        <div className="linked_site">
+          <Logo src={mathoverflow} />
+          mathoverflow.net
+        </div>
+        <div className="linked_site">
+          <Logo src={serverfault} />
+          serverfault.com
+        </div>
+        <div className="linked_site">
+          <Logo src={stackapps} />
+          stackapp.com
+        </div>
+        <div className="linked_site">
+          <Logo src={stackexchange} />
+          stackexchange.com
+        </div>
+        <div className="linked_site">
+          <Logo src={stackoverflow} />
+          stackoverflow.com
+        </div>
+        <div className="linked_site">
+          <Logo src={superuser} />
+          superuser.com
+        </div>
+        <br />
         <hr></hr>
         <div id="checkbox">
           <input type={'checkbox'}></input>
