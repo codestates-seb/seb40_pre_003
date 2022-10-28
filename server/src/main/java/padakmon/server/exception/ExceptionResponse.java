1f5f629df0cf87fc23598bad2f6fab656e308695
broden-horizon <rlghd1698@naver.com>
2022-10-28 10:16:04 +0900

    feat. Create Exception Handling AOP
    - ExceptionCode에 필요한 ExceptionCode를 추가해서, BusinessLogicException과 함께 쓰시면 됩니다.
    
    - 추가적으로 ExceptionHandling 하고 싶은 Exception이 있다면,
      1. ExceptionResponse에 of 메서드 추가로 작성한 후
      2. GlobalExceptionAdvice에 메서드에 등록해주세요.

---
+++ server/src/main/java/padakmon/server/exception/ExceptionResponse.java
@@ -0,0 +1,76 @@
+package padakmon.server.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import javax.validation.ConstraintViolationException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//에러를 클라이언트에 전달하는 DTO
+//of 메서드를 통해 여러 Exception 을 받아서 생성할 수 있다.
+//필요하다면 of 메서드 overloading 해서 사용
+//ExceptionInfo 는 어디에서 무엇이 왜 잘못했는지를 담고 있다.
+//Constructor 를 통해 생성할 수 있다.
+@Getter
+@Setter
+public class ExceptionResponse {
+    private List<ExceptionInfo> exceptionInfos;
+
+    private ExceptionResponse() {
+    }
+
+    public ExceptionResponse(List<ExceptionInfo> exceptionInfos) {
+        this.exceptionInfos = exceptionInfos;
+    }
+    //custom한 businessLogic exception 처리 코드
+    static public ExceptionResponse of(BusinessLogicException e) {
+        ExceptionResponse response = new ExceptionResponse();
+        response.setExceptionInfos(List.of(
+                new ExceptionInfo(
+                        Arrays.stream(e.getStackTrace()).findFirst().get().toString(),
+                        e.getRejectedValue(),
+                        e.getExceptionCode().getDescription()
+                )
+        ));
+        return response;
+    }
+
+    static public ExceptionResponse of(MethodArgumentNotValidException e) {
+        List<FieldError> errors = e.getBindingResult().getFieldErrors();
+        return new ExceptionResponse(
+                errors.stream().map(
+                        exception -> new ExceptionInfo(
+                              exception.getField(),
+                              exception.getRejectedValue().toString(),
+                              exception.getDefaultMessage()
+                        )
+                ).collect(Collectors.toList()));
+    }
+
+    static public ExceptionResponse of(ConstraintViolationException e) {
+        return new ExceptionResponse(
+                e.getConstraintViolations().stream().map(
+                        exception -> new ExceptionInfo(
+                                exception.getPropertyPath().toString(),
+                                exception.getInvalidValue().toString(),
+                                exception.getMessage()
+                        )
+                ).collect(Collectors.toList())
+        );
+    }
+
+
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    static public class ExceptionInfo {
+        private String where;
+        private String what;
+        private String why;
+    }
+}
