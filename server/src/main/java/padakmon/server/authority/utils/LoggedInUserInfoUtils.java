cca36c879bd79d8b57670bbad5df5542009e3b2d
broden-horizon <rlghd1698@naver.com>
2022-10-28 15:14:38 +0900

    refactor. casting 수정
     Long Wrapper를 Obeject로 casting한 후 Long Wrapper로 casting이 안됩니다. 이게 무슨일

--- server/src/main/java/padakmon/server/authority/utils/LoggedInUserInfoUtils.java
+++ server/src/main/java/padakmon/server/authority/utils/LoggedInUserInfoUtils.java
@@ -1,17 +1,18 @@
 package padakmon.server.authority.utils;
 
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import padakmon.server.exception.BusinessLogicException;
 import padakmon.server.exception.ExceptionCode;
 
 import java.util.Map;
-
+@Component
 public class LoggedInUserInfoUtils {
     public Long extractUserId() {
         Map<String, Object> claims = (Map)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if(claims == null) {
-            throw new BusinessLogicException(ExceptionCode.AUTHENTICATION_NOT_FOUNT);
+            throw new BusinessLogicException(ExceptionCode.QUESTION_NOT_FOUND);
         }
-        return (Long) claims.get("userId");
+        return ((Number)claims.get("userId")).longValue();
     }
 }
