a68ceb31648d3c3c8d989abdc3da721a82452005
callmejeje <choije401@naver.com>
2022-10-30 18:58:15 +0900

    feat: Post Answer Button 컴포넌트 생성

---
+++ client/src/components/Buttons/PostAnswerButton/style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const AnswerButton = styled.button`
+  background-color: #0995ff;
+  font-family: inherit;
+  font-weight: 500;
+  color: white;
+  /* width: auto;
+  height: 37px; */
+  padding: 10.4px;
+  margin: 0 2px;
+  border: 1px solid transparent;
+  border-radius: 3px;
+  box-shadow: inset 0 1px 0 0 hsl(0deg 0% 100% / 40%);
+  outline: none;
+  cursor: pointer;
+`;
