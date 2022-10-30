dfa100d4624276a8fad6243c04b10bd211d657c8
callmejeje <choije401@naver.com>
2022-10-30 19:02:13 +0900

    feat: Tag 컴포넌트 생성

--- client/src/components/Tag/index.js
+++ client/src/components/Tag/index.js
@@ -1,5 +1,7 @@
-function Tag(name) {
-  return <>{name}</>;
+// redux 만들면 상태로 가져올 것임
+import { Button } from './style';
+function Tag({ name }) {
+  return <Button>{name}</Button>;
 }
 
 export default Tag;
