879a3e00d12a014f27e0b4b6e16b6b3830a613ed
ALSRUD29 <dev.mnkg@gmail.com>
2022-10-31 19:06:11 +0900

    feat: 로그인헤더

--- client/src/components/Layout/GlobalNav/index.js
+++ client/src/components/Layout/GlobalNav/index.js
@@ -1,7 +1,8 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import logo from '../../../assets/stackoverflow_logo.png';
 import Search from '../Search';
 import {
+  AferLoinIcons,
   Button,
   ButtonContainer,
   Container,
@@ -13,7 +14,23 @@ import {
   SignupButton,
 } from './style';
 
+import inbox from '../../../assets/images/inbox_icon.png';
+import message from '../../../assets/images/message_icon.png';
+import question from '../../../assets/images/question_icon.png';
+import trophy from '../../../assets/images/trophy_icon.png';
+import userimg from '../../../assets/images/userimg_icon.png';
+
 function GlobalNav() {
+  const navigate = useNavigate();
+
+  //const isLogin = useSelector((state) => state.isLogin);
+  const isLogin = localStorage.getItem('accesstoken');
+  console.log('헤더 이즈로그인:', isLogin);
+  const handleClickLogout = () => {
+    navigate('/logout');
+  };
+  console.log('로컬스토리지액세스토큰', localStorage.getItem('accesstoken'));
+
   return (
     <Header>
       <Container>
@@ -28,12 +45,35 @@ function GlobalNav() {
           <Button>For Teams</Button>
         </ButtonContainer>
         <Search />
-        <RegiLink to={'/login'}>
-          <LoginButton>Log in</LoginButton>
-        </RegiLink>
-        <RegiLink to={'/signup'}>
-          <SignupButton>Sign up</SignupButton>
-        </RegiLink>
+        {isLogin !== null ? (
+          <AferLoinIcons>
+            <button>
+              <img src={userimg} alt="img" />
+            </button>
+            <button>
+              <img src={inbox} alt="img" />
+            </button>
+            <button>
+              <img src={trophy} alt="img" />
+            </button>
+            <button>
+              <img src={question} alt="img" />
+            </button>
+            <button>
+              <img src={message} alt="img" />
+            </button>
+            <button onClick={handleClickLogout}>로그아웃</button>
+          </AferLoinIcons>
+        ) : (
+          <>
+            <RegiLink to={'/login'}>
+              <LoginButton>Log in</LoginButton>
+            </RegiLink>
+            <RegiLink to={'/signup'}>
+              <SignupButton>Sign up</SignupButton>
+            </RegiLink>
+          </>
+        )}
       </Container>
     </Header>
   );
