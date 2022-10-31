3a7c8a251bdc9e7e91b2eaf8362e8ea63413e635
callmejeje <choije401@naver.com>
2022-11-01 03:04:57 +0900

    feat: proxy 설정 변경

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,9 +2,15 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    ['/auth/login', '/users', '/questions'], //proxy가 필요한 path prameter를 입력합니다.
+    [
+      '/api/auth/login',
+      '/api/users',
+      '/api/questions',
+      '/api/questions/:id',
+      '/api',
+    ], //proxy가 필요한 path prameter를 입력합니다.
     createProxyMiddleware({
-      target: 'https://c917-124-5-120-166.jp.ngrok.io/', //타겟이 되는 api url를 입력합니다.
+      target: 'https://6abe-124-5-120-166.jp.ngrok.io/', //타겟이 되는 api url를 입력합니다.
       changeOrigin: true, //대상 서버 구성에 따라 호스트 헤더가 변경되도록 설정하는 부분입니다.
     })
   );
