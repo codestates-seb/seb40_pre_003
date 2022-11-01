5e525a64cb38bae31ecce43db00c2cc1aa04456b
callmejeje <choije401@naver.com>
2022-11-01 17:26:10 +0900

    feat: proxy 필요한 path parameter /api로 수정

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,15 +2,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    [
-      '/api/auth/login',
-      '/api/users',
-      '/api/questions',
-      '/api/questions/:id',
-      '/api',
-    ], //proxy가 필요한 path prameter를 입력합니다.
+    ['/api'], //proxy가 필요한 path prameter를 입력합니다.
     createProxyMiddleware({
-      target: 'https://6abe-124-5-120-166.jp.ngrok.io/', //타겟이 되는 api url를 입력합니다.
+      target: 'https://5068-124-5-120-166.jp.ngrok.io/', //타겟이 되는 api url를 입력합니다.
       changeOrigin: true, //대상 서버 구성에 따라 호스트 헤더가 변경되도록 설정하는 부분입니다.
     })
   );
