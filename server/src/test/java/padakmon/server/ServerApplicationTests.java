b30c69cf26fcce7953b1993d8e3efe5fd11f1428
Gibeom Nam <rlqja0523@naver.com>
2022-11-02 21:41:30 +0900

    fix: 빌드 오류 수정

--- server/src/test/java/padakmon/server/ServerApplicationTests.java
+++ server/src/test/java/padakmon/server/ServerApplicationTests.java
@@ -3,7 +3,7 @@ package padakmon.server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ServerApplicationTests {
 
 	@Test
