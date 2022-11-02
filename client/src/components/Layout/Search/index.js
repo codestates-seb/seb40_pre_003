f8748c55ef17d4eb60fd5b073ec92c52dc93a860
kimhyojung <rlgywnd123@gmail.com>
2022-11-02 13:08:30 +0900

    feat: search컴포-> 코드수정

--- client/src/components/Layout/Search/index.js
+++ client/src/components/Layout/Search/index.js
@@ -1,36 +1,39 @@
 import { useState } from 'react';
 import { GoSearch } from 'react-icons/go';
 import { useNavigate } from 'react-router-dom';
-// import SearchResults from '../../../pages/SearchResults';
 import { Container, Input, SearchContainer } from './style';
+import { searchGenAction, searchTagAction } from '../../../redux';
+import { useDispatch } from 'react-redux';
 
 function Search() {
   const [searchValue, setSearchValue] = useState('');
   const navigate = useNavigate();
-
-  // 검색조건에 tag: 가 있는지 없는지 - 불린값
-  // let isSearchTag = false;
-  // console.log(searchValue);
+  const dispatch = useDispatch();
+  // const state = useSelector((state) => state);
 
   // 검색창에 tag: 이 들어갈때
   const onChangeValue = (e) => {
-    // if (e.target.value.includes('tag:')) {
-    //   console.log(e.target.value);
-    //   setSearchValue(e.target.value.slice(4));
-    //   // isSearchTag = true;
-    // }
     setSearchValue(e.target.value);
     console.log(searchValue);
-    // isSearchTag = false;
   };
 
   // tag: 가 포함된 상태로 Enter를 눌렀을때랑 아닐때 이벤트핸들러
   const handleEnter = (e) => {
     console.log(searchValue);
-    if (e.key === 'Enter' && searchValue.includes('tag:')) {
-      console.log('tag: 검색하고 엔터');
-    } else if (e.key === 'Enter' && !searchValue.includes('tag:')) {
-      console.log('일반 검색하고 엔터!');
+    if (
+      e.key === 'Enter' &&
+      searchValue.includes('[') &&
+      searchValue.includes(']')
+    ) {
+      // Questions tagged [] 로 이동!
+      dispatch(searchTagAction(searchValue.slice(1, searchValue.length - 1)));
+      console.log(
+        'Search컴포->[tag]검색->엔터->상태값-> : ',
+        searchValue.slice(1, searchValue.length - 1)
+      );
+    } else if (e.key === 'Enter' && !searchValue.includes('[')) {
+      dispatch(searchGenAction(searchValue));
+      console.log('Search컴포->일반검색->엔터->상태값-> : ', searchValue);
       navigate('/search');
     }
   };
