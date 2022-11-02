30b199b6663b46afc478c6db1a7293c12f839033
ALSRUD29 <dev.mnkg@gmail.com>
2022-11-02 11:40:38 +0900

    fix: 잘못설치된 모듈 제거

--- client/src/pages/Register/GoogleLogin/index.js
+++ client/src/pages/Register/GoogleLogin/index.js
@@ -1,37 +1,36 @@
-import { gapi } from 'gapi-script';
-import { useEffect } from 'react';
+// import { gapi } from 'gapi-script';
+// import { useEffect } from 'react';
 
-const clientId = '받아온 클라이언트 아이디';
+// const clientId = '받아온 클라이언트 아이디';
 
-//수정용
 
-useEffect(() => {
-  const initClient = () => {
-    gapi.client.init({
-      clientId: clientId,
-      scope: '',
-    });
-  };
-  gapi.load('client:auth2', initClient);
-});
+// useEffect(() => {
+//   const initClient = () => {
+//     gapi.client.init({
+//       clientId: clientId,
+//       scope: '',
+//     });
+//   };
+//   gapi.load('client:auth2', initClient);
+// });
 
-const GoogleLogin = () => {
-  const onSuccess = (res) => {
-    console.log('success:', res);
-  };
-  const onFailure = (err) => {
-    console.log('failed:', err);
-  };
-  return (
-    <GoogleLogin
-      clientId={clientId}
-      buttonText="Sign in with Google"
-      onSuccess={onSuccess}
-      onFailure={onFailure}
-      cookiePolicy={'single_host_origin'}
-      isSignedIn={true}
-    />
-  );
-};
+// const GoogleLogin = () => {
+//   const onSuccess = (res) => {
+//     console.log('success:', res);
+//   };
+//   const onFailure = (err) => {
+//     console.log('failed:', err);
+//   };
+//   return (
+//     <GoogleLogin
+//       clientId={clientId}
+//       buttonText="Sign in with Google"
+//       onSuccess={onSuccess}
+//       onFailure={onFailure}
+//       cookiePolicy={'single_host_origin'}
+//       isSignedIn={true}
+//     />
+//   );
+// };
 
-export default GoogleLogin;
+// export default GoogleLogin;
