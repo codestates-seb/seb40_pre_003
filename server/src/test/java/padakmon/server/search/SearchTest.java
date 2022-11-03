b02302da738fd5d94fe2a37dce9ebf3a3f289764
Gibeom Nam <rlqja0523@naver.com>
2022-11-04 03:18:39 +0900

    Revert "fix: 경로 수정"
    This reverts commit 9db54e21c242baff368d065d7a3b28cb98641d6b.

---
+++ server/src/test/java/padakmon/server/search/SearchTest.java
@@ -0,0 +1,50 @@
+package padakmon.server.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import padakmon.server.question.entity.Question;
+import padakmon.server.question.repository.QuestionRepository;
+
+@SpringBootTest
+public class SearchTest {
+    @Autowired
+    private QuestionRepository questionRepository;
+    @Test
+    public void search() {
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("createdAt"));
+        Page<Question> response = questionRepository.search("ABC", pageRequest);
+        System.out.println(response.getContent().get(0).getTitle());
+    }
+
+    @Test
+    public void tagSearch() {
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("createdAt"));
+        Page<Question> response = questionRepository.tagSearch("java", pageRequest);
+        System.out.println(response.getContent().get(0).getTitle());
+    }
+    @Test
+    public void userSearch() {
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("createdAt"));
+        Page<Question> response = questionRepository.userSearch(1, pageRequest);
+        System.out.println(response.getContent().get(0).getTitle());
+    }
+
+    @Test
+    public void unansweredSearch() {
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("createdAt"));
+        Page<Question> response = questionRepository.unansweredSearch(pageRequest);
+        System.out.println(response.getContent().get(0).getTitle());
+    }
+
+    @Test
+    public void answeredNumberSearch() {
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("createdAt"));
+        Page<Question> response = questionRepository.answeredNumberSearch(1, pageRequest);
+        System.out.println(response.getContent().get(0).getTitle());
+    }
+
+}
