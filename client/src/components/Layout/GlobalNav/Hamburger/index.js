0b198b1e44c260d13530416da9ee307a66488184
callmejeje <choije401@naver.com>
2022-11-04 19:18:28 +0900

    style: 햄버거 아이콘

--- client/src/components/Layout/GlobalNav/Hamburger/index.js
+++ client/src/components/Layout/GlobalNav/Hamburger/index.js
@@ -1,15 +1,37 @@
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { GrClose } from 'react-icons/gr';
 import styled from 'styled-components';
 
 function Hamburger({ hamburger, openHamburger }) {
-  return <Button onClick={openHamburger}>{hamburger ? 'x' : '='}</Button>;
+  return (
+    <Container>
+      <Button onClick={openHamburger}>
+        {hamburger ? (
+          <GrClose size={'18px'} />
+        ) : (
+          <GiHamburgerMenu size={'18px'} />
+        )}
+      </Button>
+    </Container>
+  );
 }
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0;
+`;
 
 export const Button = styled.button`
   @media screen and (min-width: 640px) {
     display: none;
   }
+  display: flex;
+  align-items: center;
   background-color: rgba(0, 0, 0, 0);
-  color: black;
+  color: #51595f;
+  margin: 0px;
+  padding: 0 12px;
 `;
 
 export default Hamburger;
