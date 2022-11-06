39a20dc2370f389ad2358acac8d39da105aefa71
Gibeom Nam <rlqja0523@naver.com>
2022-11-06 20:30:02 +0900

    fix: cors 설정 수정

--- server/src/main/java/padakmon/server/config/SecurityConfiguration.java
+++ server/src/main/java/padakmon/server/config/SecurityConfiguration.java
@@ -2,6 +2,8 @@ package padakmon.server.config;
 
 import lombok.AllArgsConstructor;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -32,13 +34,16 @@ import java.util.List;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class SecurityConfiguration {
     private final JwtTokenizer jwtTokenizer;
     private final UserAuthorityUtils userAuthorityUtils;
     private final DetailService detailService;
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
     private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Value("${config.domain}")
+    private String domain;
 
 
     @Bean
@@ -63,10 +68,10 @@ public class SecurityConfiguration {
                                 .antMatchers(HttpMethod.PATCH, "api/questions/**").hasRole("USER")
                                 .antMatchers(HttpMethod.DELETE, "api/questions/**").hasRole("USER")
                                 .anyRequest().permitAll()
-                );
-//                .oauth2Login()
-//                .successHandler(oAuth2SuccessHandler)
-//                .userInfoEndpoint().userService(customOAuth2UserService);
+                )
+                .oauth2Login()
+                .successHandler(oAuth2SuccessHandler)
+                .userInfoEndpoint().userService(customOAuth2UserService);
 
 
 
@@ -81,10 +86,11 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() { // CORS 정책
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
-//        configuration.setAllowedOrigins(List.of("http://padakmon-client-bucket.s3-website.ap-northeast-2.amazonaws.com")); // TODO S3
+        configuration.setAllowedOriginPatterns(List.of("*"));
+//        configuration.setAllowedOrigins(List.of(domain)); // TODO 배포시 S3로 변경
         configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("*"));
+//        configuration.setAllowedHeaders(List.of("*", "Authorization"));
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
