5639517497ab3dd6ef1400b4ec0ddebb08a3c4d8
ALSRUD29 <dev.mnkg@gmail.com>
2022-11-07 18:32:49 +0900

    test: 구글로그인 홈경로에서 테스트

--- client/src/pages/GooglePage/index.js
+++ client/src/pages/GooglePage/index.js
@@ -1,25 +1,25 @@
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
-const url = process.env.REACT_APP_API_URL;
+// const url = process.env.REACT_APP_API_URL;
 
 const GooglePage = () => {
-  const navigate = useNavigate();
+  //   const navigate = useNavigate();
 
-  window.location.assign(url + `/oauth2/authorization/google`);
+  //   window.location.assign(url + `/oauth2/authorization/google`);
 
-  let googleAccessToken = new URL(location.href).searchParams.get(
-    'access_token'
-  );
-  //   let googleRefreshToken = new URL(location.href).searchParams.get(
-  //     'refresh_token'
+  //   let googleAccessToken = new URL(location.href).searchParams.get(
+  //     'access_token'
   //   );
-  if (googleAccessToken) {
-    localStorage.setItem('accesstoken', googleAccessToken);
+  //   //   let googleRefreshToken = new URL(location.href).searchParams.get(
+  //   //     'refresh_token'
+  //   //   );
+  //   if (googleAccessToken) {
+  //     localStorage.setItem('accesstoken', googleAccessToken);
 
-    setTimeout(() => {
-      navigate('/');
-    }, 2000);
-  }
+  //     setTimeout(() => {
+  //       navigate('/');
+  //     }, 2000);
+  //   }
 
   return <div>관리자만 들어올 수 있는 페이지</div>;
 };
